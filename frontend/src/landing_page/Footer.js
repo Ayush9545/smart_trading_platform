61e00f5cec9aafd1c7c87f9c99f957b4ff855413
Ayush9545 <ayushgotefode@gmail.com>
2026-07-10 17:52:27 +0530

    Done footer & navbar

--- frontend/src/landing_page/Footer.js
+++ frontend/src/landing_page/Footer.js
@@ -1,9 +1,107 @@
 import React from 'react';
 
 function Footer() {
-    return ( <h1>
-        Footer
-    </h1> );
+    return (
+        <footer style={{ backgroundColor: "rgb(250, 250, 250)" }}>
+            <div className="container border-top mt-5">
+                <div className="row mt-5">
+                    <div className="col text-muted">
+                        <img src="media/images/logo.svg" style={{ width: "70%" }} className="mb-3" />
+                        <p>
+                            &copy; 2010 - 2026, Zerodha Broking Ltd. All rights reserved.
+                        </p>
+                        <img src="media/images/Screenshot1.png" style={{width: "100%"}}/>
+                    </div>
+                    <div className="col">
+                        <p>Account</p>
+                        <div className='text-muted'>
+                            <p>Open demat account</p>
+                            <p>Minor demat account</p>
+                            <p>NRI demat account</p>
+                            <p>HUF demat account</p>
+                            <p>Commodity</p>
+                            <p>Dematerialisation</p>
+                            <p>Fund transfer</p>
+                            <p>MTF</p>
+                        </div>
+                    </div>
+                    <div className="col">
+                        <p>Support</p>
+                        <div className='text-muted'>
+                            <p>Contact us</p>
+                            <p>Support portal</p>
+                            <p>How to file a complaint?</p>
+                            <p>Status of your complaints</p>
+                            <p>Bulletin</p>
+                            <p>Circular</p>
+                            <p>Z-Connect blog</p>
+                            <p>Downloads</p>
+                        </div>
+                    </div>
+                    <div className="col">
+                        <p>Company</p>
+                        <div className='text-muted'>
+                            <p>About</p>
+                            <p>Philosophy</p>
+                            <p>Press & media</p>
+                            <p>Careers</p>
+                            <p>Zerodha Cares (CSR)</p>
+                            <p>Zerodha.tech</p>
+                            <p>Open source</p>
+                            <p>Referral program</p>
+                        </div>
+                    </div>
+                    <div className="col">
+                        <p>Quick link</p>
+                        <div className='text-muted'>
+                            <p>Upcoming IPOs</p>
+                            <p>Brokerage charges</p>
+                            <p>Market holidays</p>
+                            <p>Economic calendar</p>
+                            <p>Calculators</p>
+                            <p>Markets</p>
+                            <p>Sectors</p>
+                            <p>Gift Nifty</p>
+                        </div>
+                    </div>
+                </div>
+                <div className="mt-5 text-muted" style={{ fontSize: "10px" }}>
+                    <p>
+                        Zerodha Broking Ltd.: Member of NSE, BSE, MCX & MSEI – SEBI Registration no.: INZ000031633 CDSL/NSDL: Depository services through Zerodha Broking Ltd. – SEBI Registration no.: IN-DP-431-2019 Registered Address: Zerodha Broking Ltd., #153/154, 4th Cross, Dollars Colony, Opp. Clarence Public School, J.P Nagar 4th Phase, Bengaluru - 560078, Karnataka, India. For any complaints pertaining to securities broking please write to complaints@zerodha.com, for DP related to dp@zerodha.com. Please ensure you carefully read the Risk Disclosure Document as prescribed by SEBI | ICF
+                    </p>
+
+                    <p>
+                        Procedure to file a complaint on SEBI SCORES: Register on SCORES portal. Mandatory details for filing complaints on SCORES: Name, PAN, Address, Mobile Number, E-mail ID. Benefits: Effective Communication, Speedy redressal of the grievances
+                    </p>
+
+                    <p>
+                        Smart Online Dispute Resolution | Grievances Redressal Mechanism
+                        Investments in securities market are subject to market risks; read all the related documents carefully before investing.
+                    </p>
+
+                    <p>
+                        Attention investors: 1) Stock brokers can accept securities as margins from clients only by way of pledge in the depository system w.e.f September 01, 2020. 2) Update your e-mail and phone number with your stock broker / depository participant and receive OTP directly from depository on your e-mail and/or mobile number to create pledge. 3) Check your securities / MF / bonds in the consolidated account statement issued by NSDL/CDSL every month.
+                    </p>
+
+                    <p>
+                        India's largest broker based on networth as per NSE. NSE broker factsheet
+                    </p>
+
+                    <p>
+                        "Prevent unauthorised transactions in your account. Update your mobile numbers/email IDs with your stock brokers/depository participants. Receive information of your transactions directly from Exchange/Depositories on your mobile/email at the end of the day. Issued in the interest of investors. KYC is one time exercise while dealing in securities markets - once KYC is done through a SEBI registered intermediary (broker, DP, Mutual Fund etc.), you need not undergo the same process again when you approach another intermediary." Dear Investor, if you are subscribing to an IPO, there is no need to issue a cheque. Please write the Bank account number and sign the IPO application form to authorize your bank to make payment in case of allotment. In case of non allotment the funds will remain in your bank account. As a business we don't give stock tips, and have not authorized anyone to trade on behalf of others. If you find anyone claiming to be part of Zerodha and offering such services, please create a ticket here.
+                    </p>
+
+                    <p>
+                        *Customers availing insurance advisory services offered by Ditto (Tacterial Consulting Private Limited | IRDAI Registered Corporate Agent (Composite) License No CA0738) will not have access to the exchange investor grievance redressal forum, SEBI SCORES/ODR, or arbitration mechanism for such products.
+                    </p>
+
+                    <p>
+                        Fixed deposit products offered on this platform are third-party products (TPP) and are not Exchange traded products. These are offered through Blostem Fintech Private Limited. Zerodha Broking Limited (SEBI Registration No.: INZ000031633) is acting solely as a distributor for these products. Any disputes arising with respect to such distribution activity will not have access to SEBI SCORES/ODR, Exchange Investor Grievance Redressal Forum, or Arbitration mechanism. Fixed deposits are regulated by the Reserve Bank of India (RBI).
+                    </p>
+                </div>
+            </div>
+        </footer>
+    );
 }
 
 export default Footer;
--- frontend/src/landing_page/OpenAccount.js
+++ frontend/src/landing_page/OpenAccount.js
@@ -1,11 +1,22 @@
 import React from 'react';
 
 function OpenAccount() {
-    return ( 
-        <h1>
-            OpenAccount
-        </h1>
-     );
+    return (
+        <div className="container p-5">
+            <div className="row text-center p-5">
+                <h1 className="mb-3 fs-3" >Open a Zerodha account</h1>
+                <p>
+                    Modern platforms and apps, ₹0 investments, and flat ₹20 intraday and F&O trades.
+                </p>
+                <button
+                    className="p-2 btn btn-primary fs-5"
+                    style={{ width: "20%", margin: "0 auto" }}
+                >
+                    Sign up for free
+                </button>
+            </div>
+        </div>
+    );
 }
 
 export default OpenAccount;
